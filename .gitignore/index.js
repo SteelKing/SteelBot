ad262e8405b22a34563a86d70c85c58dc373fa38
SteelKing <37270420+SteelKing@users.noreply.github.com>
2018-04-01 00:02:41 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+   console.log("LE bot a bien été connecter");
+});
+
+bot.login("NDI0OTA5NzE4MTM0MzI1MjUw.DaGNyA.NO1KUz5wMyLkjdP73Ypmt2dIkpA");
+
