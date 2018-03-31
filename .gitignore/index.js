000f877e20b1bdf377ef720578c6db96cf27d695
SteelKing <37270420+SteelKing@users.noreply.github.com>
2018-04-01 00:07:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,5 +6,5 @@ bot.on("ready", function() {
    console.log("LE bot a bien été connecter");
 });
 
-bot.login("NDI0OTA5NzE4MTM0MzI1MjUw.DaGNyA.NO1KUz5wMyLkjdP73Ypmt2dIkpA");
+bot.login(process.env.TOKEN);
 
