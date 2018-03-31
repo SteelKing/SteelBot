8cddc2c90780fd2b02f6e691d550208781d7ad96
SteelKing <37270420+SteelKing@users.noreply.github.com>
2018-04-01 00:32:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,10 +5,12 @@ var bot = new Discord.Client();
 bot.on("ready", function() {
    console.log("LE bot a bien été connecter");
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-     
+  
 bot.login(process.env.TOKEN);
+
+
+bot.on('message', function (message)) {
+    message.reply ("pong!")
+}
+   
 
